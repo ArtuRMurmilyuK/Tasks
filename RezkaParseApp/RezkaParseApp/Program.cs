786c6127139537b27ac6b6263054a9c9c05df9f0
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2021-11-30 16:54:11 +0200

    Create the project

---
+++ RezkaParseApp/RezkaParseApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RezkaParseApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
---
+++ RezkaParseApp/RezkaParseApp/readme.txt
@@ -0,0 +1,4 @@
+﻿- Parse movie Titles
+- Parse movie description
+- Parse year of issue
+- Parse genre(not one)
