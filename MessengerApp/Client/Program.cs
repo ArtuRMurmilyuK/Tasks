81b09e3d71511fc6132df4ccc5be3e3851d6a20c
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2021-12-05 08:36:21 +0200

    Add Client and Server

---
+++ MessengerApp/Client/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static int port = 8005; // порт сервера
+        static string address = "127.0.0.1"; // адрес сервера
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                // подключаемся к удаленному хосту
+                socket.Connect(ipPoint);
+                while (true)
+                {
+                    Console.Write("Введите сообщение:");
+                    string message = Console.ReadLine();
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    socket.Send(data);
+
+                    // получаем ответ
+                    data = new byte[256]; // буфер для ответа
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // количество полученных байт
+
+                    do
+                    {
+                        bytes = socket.Receive(data, data.Length, 0);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (socket.Available > 0);
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ":" + builder.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ MessengerApp/MessengerApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MessengerApp
+{
+    internal class Program
+    {
+        private static int port = 8005;
+        static void Main(string[] args)
+        {
+            //получаем адресс для запуска сокета
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+                
+            //создаем сокет
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listenSocket.Bind(ipPoint);
+
+                listenSocket.Listen(10);
+
+                Console.WriteLine("Сервер запущен. Ожидаем подключений...");
+
+                while (true)
+                {
+                    Socket handler = listenSocket.Accept();
+                    //получаем сообщение
+                    while (true)
+                    {
+                        StringBuilder builder = new StringBuilder();
+                        int bytes = 0;
+                        byte[] data = new byte[256];
+
+                        do
+                        {
+                            bytes = handler.Receive(data);
+                            builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+
+                        } while (handler.Available > 0);
+
+                        Console.WriteLine(DateTime.Now.ToShortTimeString() + ":" + builder.ToString());
+
+                        Console.Write("Введите сообщение:");
+                        string message = Console.ReadLine();
+                        data = Encoding.Unicode.GetBytes(message);
+                        handler.Send(data);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
