cadf407b82294cc74d1a7c41c6474e65eada361b
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2022-01-09 09:04:13 +0200

    Add delegates

--- Delegate/CalcDelegate/CalcDelegate/Program.cs
+++ Delegate/CalcDelegate/CalcDelegate/Program.cs
@@ -2,8 +2,26 @@
 {
     internal class Program
     {
+        public delegate int MyDelegate(int a, int b);
         static void Main()
         {
+            MyDelegate addDelegate = (a, b) => a + b;
+            MyDelegate subDelegate = (a, b) => a - b;
+            MyDelegate mulDelegate = (a, b) => a * b;
+            MyDelegate divDelegate = (a, b) =>
+            {
+                if (b != 0)
+                {
+                    return a / b;
+                }
+
+                return 0;
+            };
+
+            addDelegate.Invoke(2, 3);
+            subDelegate.Invoke(2, 3);
+            mulDelegate.Invoke(2, 3);
+            divDelegate.Invoke(2, 3);
         }
     }
 }
