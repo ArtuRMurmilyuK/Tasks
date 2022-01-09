2d413dc7b5678177fada7816c740ad7d91a71553
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2022-01-09 16:34:31 +0200

    populated an array of delegates

--- Delegate/ArrayOfDelegates/ArrayOfDelegates/Program.cs
+++ Delegate/ArrayOfDelegates/ArrayOfDelegates/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace ArrayOfDelegates
+﻿using System;
+
+namespace ArrayOfDelegates
 {
     internal class Program
     {
+        delegate int MyDelegate();
+        delegate double MyDel(MyDelegate[] a);
+
+        static int GetRandom()
+        {
+            return new Random().Next(100);
+        }
+
         static void Main(string[] args)
         {
+            var array = new MyDelegate[3];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = () => new MyDelegate(GetRandom).Invoke();
+            }
+
+            MyDel del = delegate(MyDelegate[] c)
+            {
+                int sum = 0;
+                for (int i = 0; i < c.Length; i++)
+                {
+                    sum += c[i].Invoke();
+                }
+
+                return sum / array.Length;
+            };
+
+            Console.WriteLine(del(array));
         }
     }
 }
