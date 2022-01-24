7e7a9043ee374e39c0bbf51a6babe491166ab446
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2022-01-24 20:03:24 +0200

    Add ParticipantsDB

--- ListOfParticipants/ListOfParticipants/Program.cs
+++ ListOfParticipants/ListOfParticipants/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace ListOfParticipants
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ListOfParticipants
 {
     internal class Program
     {
         static void Main()
         {
+        }
+    }
+
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class AppContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb; Database=ParticipantsDB; Trusted_Connection=True");
         }
     }
 }
