7dcddf3810e164809b07e505ac9751ae5ebef684
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2022-01-24 19:59:38 +0200

    Add DB

---
+++ YouTubeParse/YouTubeParse/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.WebSockets;
+using Leaf.xNet;
+using Fizzler;
+using Fizzler.Systems.HtmlAgilityPack;
+using HtmlAgilityPack;
+using HtmlDoc = HtmlAgilityPack.HtmlDocument;
+
+namespace YouTubeParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parse parse = new Parse();
+            parse.InitHttp();
+        }
+    }
+
+    public class Parse
+    {
+        public void InitHttp()
+        {
+            var httpReq = new HttpRequest();
+
+            string response = httpReq.Get(Console.ReadLine()).ToString();
+
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            var html = new HtmlDocument();
+            var document = html.DocumentNode;
+            var innerHtml = document.QuerySelector(".title").InnerHtml;
+            Console.WriteLine(innerHtml);
+
+            File.WriteAllText("test.html", response, Encoding.UTF8);
+            Process.Start("test.html");
+            Console.ReadKey();
+        }
+
+    }
+}
