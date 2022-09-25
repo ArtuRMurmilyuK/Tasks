8f7a6f68a993157e38330c8c7b47d54b0a8322ce
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2022-09-25 14:19:02 +0300

    Added Thread

--- ThreadLesson1/ThreadLesson1/Program.cs
+++ ThreadLesson1/ThreadLesson1/Program.cs
@@ -1,9 +1,90 @@
-﻿namespace ThreadLesson1
+﻿using System;
+using System.Threading;
+
+namespace ThreadLesson1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Для запуска нажмите любую кнопку");
+            Console.ReadKey();
+
+            ThreadPoolWorker threadPoolWorker = new ThreadPoolWorker(new Action<object>(StarWriter));
+            threadPoolWorker.Start('*');
+
+            for(int i = 0; i < 40; i++)
+            {
+                Console.Write('-');
+                Thread.Sleep(50);
+            }
+
+            threadPoolWorker.Wait();
+
+            Console.WriteLine("Метод Main закончил свою работу");
+        }
+
+        private static void StarWriter(object arg)
+        {
+            char item = (char)arg;
+
+            for(int i = 0; i < 120; i++)
+            {
+                Console.Write(item);
+                Thread.Sleep(50);
+            }
+        }
+    }
+    
+    public class ThreadPoolWorker
+    {
+        private readonly Action<object> _action;
+
+        public ThreadPoolWorker(Action<object> action)
+        {
+            _action = action ?? throw new ArgumentNullException(nameof(action));
+        }
+
+        public bool IsCompleted { get; private set; } = false;
+        public bool IsSuccess { get; private set; } = false;
+        public bool IsFaulted { get;private set; } = false;
+        public Exception Exception { get; private set; } = null;
+
+        public void Start(object state)
+        {
+            ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadExecution), state);
+        }
+
+        public void Wait()
+        {
+            while(IsCompleted == false)
+            {
+                Thread.Sleep(150);
+            }
+
+            if(Exception != null)
+            {
+                throw Exception;
+            }
+        }
+
+        private void ThreadExecution(object state)
+        {
+            try
+            {
+                _action.Invoke(state);
+                IsSuccess = true;
+            }
+            catch(Exception ex)
+            {
+                Exception = ex;
+                IsSuccess = false;
+                IsFaulted = true;
+            }
+            finally
+            {
+                IsCompleted = true;
+            }
         }
     }
 }
