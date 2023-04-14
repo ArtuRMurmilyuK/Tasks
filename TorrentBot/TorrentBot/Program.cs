cbfb56504e971ba909f1d10691c308e78b4c66e3
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2023-04-14 17:34:02 +0300

    Added save in folder

--- TorrentBot/TorrentBot/Program.cs
+++ TorrentBot/TorrentBot/Program.cs
@@ -3,11 +3,9 @@ using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.ReplyMarkups;
 using TorrentBot;
-using static TorrentBot.Keyboards;
 
-var botClient = new TelegramBotClient("");
+var botClient = new TelegramBotClient("6030647103:AAE1tsj6JipdgFxjYzeODdf9bw4k9dwSVLo");
 
 using CancellationTokenSource cts = new();
 
@@ -32,36 +30,37 @@ Console.ReadLine();
 // Send cancellation request to stop bot
 cts.Cancel();
 
-//if (fileName.EndsWith(".torrent"))
-{
-    Console.WriteLine("File has .torrent extension");
-}
 async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
 {
-    switch (update.Message.Text)
+    if (update.Message.Type == MessageType.Text)
     {
-        case "/start":
-            await botClient.SendTextMessageAsync(update.Message.Chat.Id, $"🛠⚙ Welcome to QbitTorren Bot 🛠⚙");
-            await botClient.SendTextMessageAsync(
-                chatId: update.Message.Chat.Id,
-                text: "How can I help you?",
-                replyMarkup: Keyboards.MainMenuKeyboard(),
-                cancellationToken: cancellationToken);
-            break;
-        case "Info":
-            await botClient.SendTextMessageAsync(update.Message.Chat.Id, $"With my help you can download torrent files directly to your device");
-            break;
-        case "Send File":
-            await botClient.SendTextMessageAsync(
-                 chatId: update.Message.Chat.Id,
-                 text: "Upload your file here",
-                 replyMarkup: new ReplyKeyboardRemove(),
-                 cancellationToken: cancellationToken);
-            
-            break;
+        switch (update.Message.Text)
+        {
+            case "/start":
+                await botClient.SendTextMessageAsync(update.Message.Chat.Id, $"🛠⚙ Welcome to QbitTorren Bot 🛠⚙");
+                await botClient.SendTextMessageAsync(
+                    chatId: update.Message.Chat.Id,
+                    text: "How can I help you?",
+                    replyMarkup: Keyboards.MainMenuKeyboard(),
+                    cancellationToken: cancellationToken);
+                break;
+            case "Info":
+                await botClient.SendTextMessageAsync(update.Message.Chat.Id, $"With my help you can download torrent files directly to your device");
+                break;
+        }
     }
+    else
+    {
+        if (update.Message.Caption != null)
+        {
+            DownloadFileAsync(botClient, update, update.Message.Caption);
+        }
+    }
+}
 
-    if(update.Message.Type == Telegram.Bot.Types.Enums.MessageType.Document)
+async void DownloadFileAsync(ITelegramBotClient botClient, Update update, string path)
+{
+    if (update.Message.Type == Telegram.Bot.Types.Enums.MessageType.Document)
     {
         if (update.Message.Document.FileName.EndsWith(".torrent"))
         {
@@ -69,7 +68,7 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
             var file = await botClient.GetFileAsync(update.Message.Document.FileId);
 
             // сохраняем файл в определенную папку
-            string filePath = Path.Combine("C:\\Users\\Murmi\\Documents\\", update.Message.Document.FileName);
+            string filePath = Path.Combine($"C:\\Users\\Murmi\\Documents\\{path}\\", update.Message.Document.FileName);
             using (var fileStream = new FileStream(filePath, FileMode.Create))
             {
                 await botClient.DownloadFileAsync(file.FilePath, fileStream);
@@ -84,7 +83,7 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
             await botClient.SendTextMessageAsync(update.Message.Chat.Id, $"File has .torrent extension");
         }
     }
-
+    
 }
 
 Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
