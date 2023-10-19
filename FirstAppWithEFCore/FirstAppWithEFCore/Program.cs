cd9ec1906e44c7846aa853255333504b3ed565cb
ArtuRMurmilyuK <ArtuRMurmilyuK>
2023-10-19 20:13:34 +0300

    Added Db, model User

--- FirstAppWithEFCore/FirstAppWithEFCore/Program.cs
+++ FirstAppWithEFCore/FirstAppWithEFCore/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using FirstAppWithEFCore;
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    User tom = new User { Name = "Tom", Age = 33};
+    User alice = new User { Name = "Alice", Age = 24 };
+
+    db.Users.Add(tom);
+    db.Users.Add(alice);
+    db.SaveChanges();
+
+    Console.WriteLine("Object added successfully");
+    
+    var users = db.Users.ToList();
+    Console.WriteLine("Object list: ");
+    foreach (User user  in users)
+    {
+        Console.WriteLine($"{user.Id}.{user.Name} - {user.Age}");
+    }
+}
